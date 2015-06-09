80301a0d047725d675978fb6d19c134c6912fa18
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2015-06-09 18:31:52 +0300

    Implemented number/atan.

--- src/number/atan.js
+++ src/number/atan.js
@@ -8,4 +8,5 @@
 export function atan (
 
 ) : number {
+    return Math.atan(this);
 };
---
+++ test/spec/number/atanSpec.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import { atan } from "../../../src/number/atan";
+
+describe("atan()", function () {
+    it("should return the arctangent of the number", function () {
+        (+1.0)::atan().should.equal(Math.atan(+1.0));
+        (+0.5)::atan().should.equal(Math.atan(+0.5));
+        (+0.0)::atan().should.equal(Math.atan(+0.0));
+        (-0.5)::atan().should.equal(Math.atan(-0.5));
+        (-1.0)::atan().should.equal(Math.atan(-1.0));
+    });
+});
