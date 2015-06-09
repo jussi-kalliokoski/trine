281ccf8b02640685d08bf87a47e454512f32c36c
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2015-06-09 17:26:56 +0300

    Implemented function/partial.

--- src/function/partial.js
+++ src/function/partial.js
@@ -1,6 +1,26 @@
 "use strict";
 
 /**
+ * Used for partial application to denote that this slot expects an
+ * argument from the resulting function's calls.
+*/
+export const _ = Symbol("placeholder");
+
+/**
+ * Used for partial application to denote that this slot expects zero or
+ * more arguments from the resulting function's calls.
+*/
+export const ___ = Symbol("rest placeholder");
+
+function countPlaceholders () {
+    return this.filter((param) => param === _).length;
+}
+
+function flatMap (transformer) {
+    return [].concat.apply([], this.map(transformer));
+}
+
+/**
  * Returns a version of the function that has given parameters prefilled and passes given parameters through to the original, denoted by placeholders.
  *
  * @this {T}
@@ -48,7 +68,34 @@
  * [1, 2, 3, 4]::slice1() // [2, 3, 4]
  * ```
 */
-export function partial <T> (
-    ...staticParams : any,
-) : T {
+export function partial <T> (...staticParams : any) : T {
+    const delegate = this;
+    const restIndex = staticParams.indexOf(___);
+    const placeholdersAfterRest = staticParams
+        .slice(restIndex === -1 ? staticParams.length : restIndex + 1)
+        ::countPlaceholders();
+
+    if ( staticParams.indexOf(___, restIndex + 1) !== -1 ) {
+        throw new TypeError("You can only pass one rest placeholder to partial.");
+    }
+
+    return function (...params) {
+        const passedParams = staticParams::flatMap((param, index) => {
+            if ( param === _ ) {
+                if ( params.length > 0 ) {
+                    return [params.shift()];
+                }
+            } else if ( param === ___ ) {
+                if ( params.length > placeholdersAfterRest ) {
+                    return params.splice(0, params.length - placeholdersAfterRest);
+                }
+            } else {
+                return [param];
+            }
+
+            return [];
+        });
+
+        return delegate.apply(this, passedParams);
+    };
 };
---
+++ test/spec/function/partialSpec.js
@@ -0,0 +1,93 @@
+"use strict";
+
+import { partial, _, ___ } from "../../../src/function/partial";
+
+function mock (...params) {
+    return { this, params };
+}
+
+describe("partial()", function () {
+    it("should pass `this` to the delegate", function () {
+        "foo"::(mock::partial())().this.should.equal("foo");
+    });
+
+    describe("when given more than one rest placeholder", function () {
+        it("should throw", function () {
+            void function () {
+                mock::partial(___, 1, ___);
+            }.should.throw();
+        });
+    });
+
+    describe("when given no parameters", function () {
+        it("should pass no parameters to the delegate", function () {
+            mock::partial()(1).params.should.deep.equal([]);
+        });
+    });
+
+    describe("when given one parameter", function () {
+        it("should only pass that to the delegate", function () {
+            mock::partial(0)(1).params.should.deep.equal([0]);
+        });
+    });
+
+    describe("when given one placeholder", function () {
+        it("should only pass parameter at that index to the delegate", function () {
+            mock::partial(_)(0, 1).params.should.deep.equal([0]);
+        });
+    });
+
+    describe("when a placeholder is not filled", function () {
+        it("should not pass a parameter at that place", function () {
+            mock::partial(0, _)().params.should.deep.equal([0]);
+        });
+    });
+
+    describe("when a rest placeholder is not filled", function () {
+        it("should not pass a parameter at that place", function () {
+            mock::partial(0, _, ___, _, 1)(2, 3).params.should.deep.equal([0, 2, 3, 1]);
+        });
+    });
+
+    describe("when a rest placeholder and a placeholder are not filled", function () {
+        it("should not pass parameters at either place", function () {
+            mock::partial(___, 1, _)().params.should.deep.equal([1]);
+        });
+    });
+
+    describe("when given one parameter followed by a placeholder", function () {
+        it("should pass the parameter to the delegate, letting one parameter through", function () {
+            mock::partial(0, _)(1).params.should.deep.equal([0, 1]);
+        });
+    });
+
+    describe("when given one placeholder followed by a parameter", function () {
+        it("should pass the parameter to the delegate, letting one parameter through", function () {
+            mock::partial(_, 0)(1).params.should.deep.equal([1, 0]);
+        });
+    });
+
+    describe("when given a parameter, followed by a rest placeholder", function () {
+        it("should pass the parameter to the delegate, letting all parameters through", function () {
+            mock::partial(0, ___)(1, 2, 3).params.should.deep.equal([0, 1, 2, 3]);
+        });
+    });
+
+    describe("when given a rest placeholder, followed by a parameter", function () {
+        it("should pass the parameter as last to the delegate, letting all parameters through", function () {
+            mock::partial(___, 0)(1, 2, 3).params.should.deep.equal([1, 2, 3, 0]);
+        });
+    });
+
+    describe("when given a rest placeholder in between", function () {
+        it("should let the input parameters through in between the passed parameters", function () {
+            mock::partial(0, ___, 1)(2, 3, 4).params.should.deep.equal([0, 2, 3, 4, 1]);
+        });
+    });
+
+    describe("when given a mix of placeholders, rest placeholders and parameters", function () {
+        it("should place the parameters in the correct slots", function () {
+            mock::partial(_, 0, _, ___, 1, _, 2)(3, 4, 5, 6, 7).params.should.deep.equal([3, 0, 4, 5, 6, 1, 7, 2]);
+        });
+    });
+});
