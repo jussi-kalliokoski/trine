723e9e1ef82378fe30d54e61cd662364b8d42d9c
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2015-06-09 19:26:28 +0300

    Implemented number/sub.

--- src/number/sub.js
+++ src/number/sub.js
@@ -15,4 +15,5 @@
 export function sub (
     b : number,
 ) : number {
+    return this - b;
 };
---
+++ test/spec/number/subSpec.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import { sub } from "../../../src/number/sub";
+
+describe("sub()", function () {
+    it("should return the subtraction of the numbers", function () {
+        (5)::sub(-5).should.equal(10);
+        (-5)::sub(5).should.equal(-10);
+        (5)::sub(5).should.equal(0);
+    });
+});
