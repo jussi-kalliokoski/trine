a6b728fd7b6043c7c695bbb9fbe80038277d456f
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2015-06-09 13:48:05 +0300

    Implemented iterable/convolve.

--- src/iterable/convolve.js
+++ src/iterable/convolve.js
@@ -4,7 +4,9 @@
  * Maps the iterator with the additional tail of `k` previous items.
  * `k` is determined by the number of items in the accumulation iterator.
  *
- * @this {Iterable<T>}
+ * @type rT The item type of the returned iterator.
+ * @type iT The item type of the input iterator.
+ * @this {Iterable<iT>}
  * @ntime O(n)
  * @dspace O(k)
  * @example Basic Usage
@@ -15,8 +17,13 @@
  * }, [0, -1]) // yields 0, 3, 6
  * ```
 */
-export function * convolve (
-    accumulator : (accumulation : rT, item : iT) => rT,
+export function * convolve <rT, iT> (
+    accumulator : (_this : iT, tail : rT) => rT,
     tail : Iterable<iT>,
 ) {
+    for ( const item of this ) {
+        const result = item::accumulator(tail);
+        tail = [item].concat(tail.slice(0, tail.length - 1));
+        yield result;
+    }
 };
