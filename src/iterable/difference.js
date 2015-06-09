1693f2f2c5779532823b6b02a3cf3d7be6568459
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2015-06-09 14:10:28 +0300

    Implemented iterable/difference.

--- src/iterable/difference.js
+++ src/iterable/difference.js
@@ -4,7 +4,9 @@
  * Yields the sorted difference iterator of two sorted iterators.
  *
  * @this {Iterable<T>}
- * @param comparator The sorting value function. Should return `0` when items are equal, a positive number when the item on the left is greater and a negative number when the item on the right is greater.
+ * @param comparator The sorting value function.
+ * Should return `0` when items are equal, a positive number when the item
+ * on the left is greater and a negative number when the item on the right is greater.
  * @ntime O(n+m)
  * @dspace O(2)
  * @example Basic Usage
@@ -19,4 +21,31 @@ export function * difference <T> (
     b : Iterable<T>,
     comparator : (item: T) => number,
 ) {
+    const iteratorA = this[Symbol.iterator]();
+    const iteratorB = b[Symbol.iterator]();
+    let stepA = iteratorA.next();
+    let stepB = iteratorB.next();
+
+    while ( !stepA.done && !stepB.done ) {
+        const comparison = stepA.value::comparator(stepB.value);
+
+        if ( comparison < 0 ) {
+            yield stepA.value;
+            stepA = iteratorA.next();
+        } else if ( comparison > 0 ) {
+            yield stepB.value;
+            stepB = iteratorB.next();
+        } else {
+            stepA = iteratorA.next();
+            stepB = iteratorB.next();
+        }
+    }
+
+    if ( !stepA.done ) {
+        yield stepA.value;
+        yield * iteratorA;
+    } else if ( !stepB.done ) {
+        yield stepB.value;
+        yield * iteratorB;
+    }
 };
