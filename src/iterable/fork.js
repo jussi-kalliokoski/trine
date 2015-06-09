a8461ee9382482301f0dcf784e34504473c4e9e9
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2015-06-09 14:51:46 +0300

    Implemented iterable/fork.

--- src/iterable/fork.js
+++ src/iterable/fork.js
@@ -18,4 +18,49 @@
 export function * fork <T> (
 
 ) : Iterable<Iterable<T>> {
+    const source = this[Symbol.iterator]();
+    const indices = [];
+    let buffer = [];
+    let readIndex = 0;
+    let done = false;
+
+    while ( true ) {
+        if ( readIndex > 0 ) {
+            throw new TypeError("Can't fork the iterator after the iteration has started");
+        }
+
+        const iteratorIndex = indices.length;
+        indices.push(0);
+
+        yield function * () {
+            while ( true ) {
+                const offset = indices[iteratorIndex] - readIndex;
+
+                if ( offset < 0 ) {
+                    indices[iteratorIndex] = readIndex;
+                    const chunk = buffer.slice(offset);
+                    const lowestOffset = Math.min(...indices) - readIndex;
+
+                    if ( lowestOffset < 0 ) {
+                        buffer = buffer.slice(lowestOffset);
+                    } else {
+                        buffer = [];
+                    }
+
+                    yield * chunk;
+                    continue;
+                }
+
+                if ( done ) { return; }
+
+                const step = source.next();
+                done = step.done;
+
+                if ( done ) { return; }
+
+                buffer.push(step.value);
+                readIndex += 1;
+            }
+        }();
+    }
 };
