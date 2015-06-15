1ed8a88637675744dbd8386d5efbe6187e2562e0
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2015-06-15 18:30:42 +0300

    Made iterable/difference take both iterables from `this`.
    See #20.

--- test/spec/iterable/differenceSpec.js
+++ test/spec/iterable/differenceSpec.js
@@ -4,8 +4,28 @@ import { difference } from "../../../src/iterable/difference";
 
 describe("difference()", function () {
     it("should yield the differences of two sorted iterators", function () {
-        [...[1,2,4]::difference([2,3,4,5], function (b) {
+        [...[[1,2,4], [2,3,4,5]]::difference(function (b) {
             return this - b;
         })].should.deep.equal([1,3,5]);
+    });
+
+    describe("when provided less than 2 iterators", function () {
+        it("should throw", function () {
+            void function () {
+                [...[[1]]::difference(function (b) {
+                    return this - b;
+                })];
+            }.should.throw();
+        });
+    });
+
+    describe("when provided more than 2 iterators", function () {
+        it("should throw", function () {
+            void function () {
+                [...[[1], [2], [3]]::difference(function (b) {
+                    return this - b;
+                })];
+            }.should.throw();
+        });
     });
 });
